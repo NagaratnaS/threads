fd72ff39508776a6d2513b23ad6c6441b198a53d
Nagaratna <hegdenagaratna34@gmail.com>
2021-06-03 09:55:11 +0530

    test commit

--- src/com/wolken/wolkenapp/Multiple1.java
+++ src/com/wolken/wolkenapp/Multiple1.java
@@ -6,7 +6,7 @@ public class Multiple1 extends Thread{
 		System.out.println("Thread id:" +Thread.currentThread().getId());
 		System.out.println("Thread Name:" +Thread.currentThread().getName());
 		
-		int num = 13;
+		int num = 10;
 		for(int i = 1 ; i <= 10 ; i++) {
 			System.out.println(num +" * " + i +" = " +(num*i));
 		}
